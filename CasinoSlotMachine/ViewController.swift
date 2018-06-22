9ca185dc3280eca1086b991796a6f9288eb127ae
Anes Mehai <nsamehai@gmail.com>
2018-06-22 16:08:26 -0400

    logic for winning bets

--- CasinoSlotMachine/ViewController.swift
+++ CasinoSlotMachine/ViewController.swift
@@ -22,12 +22,31 @@ class ViewController: UIViewController {
         let num3:Int = Int(arc4random_uniform(4))
         
         //creates an ArrayList that stores the card suit images
-        var suitArray = [UIImage(named: "Club.jpg"), UIImage(named: "Diamond.jpg"), UIImage(named: "Heart.jpg"), UIImage(named: "Spade.jpg")]
+        var suitArray = [
+            UIImage(named: "Club.jpg"),
+            UIImage(named: "Diamond.jpg"),
+            UIImage(named: "Heart.jpg"),
+            UIImage(named: "Spade.jpg")
+        ]
             
         //uses the random numbers as an index from the ArrayList and reassigns the imgSlots images to random ones
         imgSlot1.image = suitArray[num1]
         imgSlot2.image = suitArray[num2]
         imgSlot3.image = suitArray[num3]
+        
+    }
+    
+    func allClubs(num1: Int, num2: Int, num3: Int){
+        
+        if(num1 == 0 && num2 == 0 && num3 == 0)
+        {
+            //10* the bet: money + (bet*10)
+            
+        } else {
+            
+            //lose: money - bet
+        }
+        
         
     }
 
